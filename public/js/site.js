6d547d433459180847c231bea360cb5b94b69db0
Luca Ryholt <lucaryholt@gmail.com>
2020-09-21 21:39:36 +0200

    fs - done

--- public/js/site.js
+++ public/js/site.js
@@ -8,7 +8,7 @@ function getWisdoms() {
         for (let i = 0; i < data.length; i++) {
             appendWisdom(data[i]);
         }
-        specificWisdom(data[7].title);
+        specificWisdom(data[3].title);
     });
 }
 
