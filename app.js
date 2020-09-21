87a72a6128e4f70c2cbd813e5615199d0eb5b709
Luca Ryholt <lucaryholt@gmail.com>
2020-09-21 18:21:34 +0200

    rest - done. images local now

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require('ejs');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 const wisdoms = sortWisdoms(require('./wisdoms/wisdoms.json'));
